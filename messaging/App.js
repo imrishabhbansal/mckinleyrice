339069c692987fa6494126c52e24f1fe6409a7f4
Rishabh Bansal <rishab.bansal400@gmail.com>
2020-07-29 16:38:27 +0530

    Update App.js

--- messaging/App.js
+++ messaging/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import LoginPage from './components/loginPage';
+import LoginPage from './loginPage';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function login(name,password) {
