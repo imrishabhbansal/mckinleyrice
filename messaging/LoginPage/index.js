17f086d77e9cba5bd85ce472662c9b8e6a8a73a3
Rishabh Bansal <rishab.bansal400@gmail.com>
2020-07-29 16:40:16 +0530

    Create index.js

---
+++ messaging/LoginPage/index.js
@@ -0,0 +1,37 @@
+.loginPage{
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    color: white
+}
+.content{
+    display: flex;
+    justify-content: center;
+    flex-flow: column wrap;
+    align-items: center;
+    height: 350px;
+    width: 350px;
+    background-color: #333338;
+    border-radius: 25%;
+}
+.loginPage input{
+    border: 1px solid #bbbaba;
+    border-radius: 22px;
+    padding: 14px;
+    padding-left: 44px;
+}
+.loginPage input:focus {outline:0;}
+.loginPage h3{
+    margin-top: 0;
+}
+.loginPage button{
+    color: white;
+    background: #ff0000d4;
+    padding: 10px;
+    margin: 10px;
+    border: 3px solid #ff0023;
+    border-radius: 7px;
+}
+.loginPage button:focus {outline:0;}
